2f9a78c78fe8e0fb31173ca3aa1d4a12f3d0db2a
Carlos Perez <carlos3786@hotmail.com>
2022-10-14 16:04:32 -0500

    Component navbar

--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,10 +10,24 @@ export class RegisterComponent implements OnInit {
   
   register:FormGroup;
   constructor(private fb:FormBuilder) { 
-    this.register=fb.group({});
+    this.register=fb.group({
+      usuario:['',Validators.required],
+      password:['',Validators.required,Validators.minLength(4)],
+      confirmPassword:['']
+    },
+    {validator:this.checkPassword}
+    );
   }
 
   ngOnInit(): void {
   }
 
+  registrarUsuario():void{
+    console.log(this.register);
+  }
+  checkPassword(group:FormGroup):any{
+    const pass= group.controls.password.value;
+    const confirmPass=group.controls.confirmPassword.value;
+    return pass===confirmPass?null:{notSame:true}
+  }
 }
